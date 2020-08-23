2caed0d537694a32264fc99f549cd021abdc7030
Saurav Dutta <saurav@piktorlabs.com>
2020-08-23 22:55:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/shop" component={ShopPage}/>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
+      </Switch>
+    </div>
   );
 }
 
